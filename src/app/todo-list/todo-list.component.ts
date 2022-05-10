4b7354aa142f0b60a26d351051af228cdc51caa9
Priyanshu Belwal <priyanshubelwalpb@gmail.com>
2022-05-10 21:29:49 +0530

    All Todos UI Screen is Ready

---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css']
+})
+export class TodoListComponent implements OnInit {
+
+  constructor() { }
+
+  todos: any[] = [
+    { task: 'Need to go home', date: '12-02-2022' },
+    { task: 'Walk', date: '15-02-2022' },
+    { task: 'Need to bath', date: '13-02-2022' },
+    { task: 'Take Food', date: '14-02-2022' },
+  ]
+
+  ngOnInit(): void {
+  }
+
+}
